a77d3455b1c062cd4eb528dcf54bf0ef94904767
Cris Paul Lastrollo <millarecrispaul@gmail.com>
2026-05-08 01:21:48 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,30 @@
 // Basketball shoe products
 const products = [
-  {id:1, name:"Anta Kai 1", price:7995, image:"images/AntaKai1.jpg"},
-  {id:2, name:"D'Lo 1", price:8595, image:"images/Dlo1.jpg"},
-  {id:3, name:"Flyby Mid 3", price:2995, image:"images/FlybyMid3.jpg"},
-  {id:4, name:"Giannis Immortality 4", price:4295, image:"images/GiannisImmortality4.jpg"},
+  {
+    id:1, 
+    name:"Anta Kai 1", 
+    price:7995, 
+    image:"images/AntaKai1.jpg"
+  },
+  
+  {
+    id:2, 
+    name:"D'Lo 1", 
+    price:8595, 
+    image:"images/Dlo1.jpg"
+  },
+  
+  {
+    id:3, 
+    name:"Flyby Mid 3", 
+    price:2995, 
+    image:"images/FlybyMid3.jpg"
+  },
+  
+  {id:4, 
+   name:"Giannis Immortality 4", 
+   price:4295, 
+   image:"images/GiannisImmortality4.jpg"
+  },
+  
 ];
